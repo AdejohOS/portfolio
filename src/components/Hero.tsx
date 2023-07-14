a528db8086c3fb9deddf6fee4b70d77c5edf1300
AdejohOS <elusivebrown@gmail.com>
2023-07-14 12:49:28 +0100

    added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,28 +4,30 @@ import { Button } from './ui/Button'
 
 const Hero = () => {
   return (
-    <div className='grid col-2'>
-        <div className='space-y-4'>
-          <h1 className='font-bold text-5xl'>Sunday Adejoh</h1>
-          <p className='text-3xl'>Fullstack Developer 👋</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, eaque fuga distinctio soluta 
-            minima mollitia sapiente optio dolores quidem voluptatum!</p>
-          <Button>
-            Download Resume
-            <FileBarChart className='pl-2' />
-          </Button>
-        </div>
+    <div className='w-full h-full mt-8'>
+        <div className='md:grid md:grid-cols-2 md:items-center
+                        gap-2 '> 
+          <div className='text-center md:text-left  space-y-4 mb-8'> 
+            <h1 className='font-bold text-5xl'>Sunday Adejoh</h1>
+            <p className='text-3xl'>Fullstack Developer 👋</p>
+            <p>With a passion for building highly scalable and efficient applications,
+              with industry best practice and tools.</p>
+            <Button>
+              Download Resume
+              <FileBarChart className='pl-2' />
+            </Button>
+          </div>
 
-        <div className='max-w-[899px] max-h-[899px] block'>
-          <Image
-            height={200}
-            width={200}
-            src={'/images/propic.jpg'}
-            alt='Profile pic'
-            className='object-cover'
-          />
-        </div>
-    </div>
+          <div className='relative'>
+            
+
+            <img src="/images/about.jpg" alt="" />
+            
+         
+          </div>
+      </div> 
+    </div> 
+    
   )
 }
 
