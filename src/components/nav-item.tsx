dba86c1280acb9c14dba196efd2f4f0bd4a84343
AdejohOS <elusivebrown@gmail.com>
2026-03-16 15:54:34 +0100

    framer

--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -2,7 +2,6 @@
 
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { motion } from 'framer-motion'
 
 interface NavItemProps {
@@ -11,27 +10,17 @@ interface NavItemProps {
 }
 
 const NavItem = ({ href, name }: NavItemProps) => {
-  const pathname = usePathname()
-
-  const isActive =
-    pathname === href || pathname.startsWith(`${href}/`)
-
   return (
     <motion.li whileHover={{ y: -2 }}>
       <Link
         href={href}
-        className={cn(
-          'relative text-sm font-medium transition-colors hover:text-[#3A6D8C]',
-          isActive && 'text-[#3A6D8C] font-semibold'
-        )}
+        className='group relative text-sm font-medium transition-colors hover:text-[#3A6D8C]'
       >
         {name}
 
-        {/* animated underline */}
         <span
           className={cn(
-            'absolute -bottom-1 left-0 h-[2px] w-0 bg-[#FFF799] transition-all duration-300',
-            isActive && 'w-full'
+            'absolute -bottom-1 left-0 h-[2px] w-0 bg-[#FFF799] transition-all duration-300 group-hover:w-full'
           )}
         />
       </Link>
